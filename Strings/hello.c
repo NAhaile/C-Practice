ea7f3eadaf362a7ca6064a6cef2e981fe8a92b36
Natnael <nhaile2@masonlive.gmu.edu>
2020-01-28 23:42:47 -0500

    Update hello.c

--- Strings/hello.c
+++ Strings/hello.c
@@ -1,6 +1,9 @@
 /* Simple C Program to Print Hello World */
 #include <stdio.h> 
 
+/* Similarly you can change the data type and make it float, and just return float
+*/
+
 int main() {
 
     printf("Hello World!! \n");
