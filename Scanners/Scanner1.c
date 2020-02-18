fc15187433183862e08a588196505800f867f9bf
Natnael <nhaile2@masonlive.gmu.edu>
2020-02-17 22:37:13 -0500

    Update Scanner1.c

--- Scanners/Scanner1.c
+++ Scanners/Scanner1.c
@@ -6,13 +6,20 @@ int main() {
 
         char buffer[100];
         char word[];
+        int number;
 
 
         printf("Word? ");
         fgets(buffer, sizeof(buffer), stdin);
         sscanf(buffer, "%c", &word);
+        
+        
+        printf("Number? ")l
+        fgets(buffer, sizeof(buffer), stdin);
+        sscanf(buffer, "%d", &number);
 
-                printf("Your Word %c Was:\n", word);
+                printf("Your Word Was: %c \n", word);
+                printf("Your number Was: %d \n", number);
 
 
         return 0;
