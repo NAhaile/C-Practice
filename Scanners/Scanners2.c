a4a34eee656e4cc96dd5938a44c36bf72d93d050
Natnael <nhaile2@masonlive.gmu.edu>
2020-03-04 23:35:13 -0500

    Add files via upload

---
+++ Scanners/Scanners2.c
@@ -0,0 +1,23 @@
+// this program is to raise any inputted number to the power of 2
+// also note, program will not compile if the -lm flag is not included
+// because the pow function has to be linked with the math library
+
+#include <stdio.h> 
+#include <math.h>
+
+int main() {
+	double b;
+	double var;
+	char buff[20];
+
+	printf("What is your Base Value? ")
+	fgets(buff, 20, stdin);
+	sscanf(buff, "%lf", &b);
+  
+
+	var = pow(b,2);
+
+	printf("Here is your number to the power of two: %.0lf", var);
+    return 0;
+    
+}
